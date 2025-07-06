f906ab31fc397d3564d1e5ae5f46839a26cf4e8d
henrique <henriquepermartins@gmail.com>
2025-07-06 17:45:52 -0300

    feat: integrate AWS S3 for file storage with upload, download, and delete functionalities

--- apps/server/src/shared/infrastructure/env.ts
+++ apps/server/src/shared/infrastructure/env.ts
@@ -4,6 +4,12 @@ const envSchema = z.object({
   NODE_ENV: z.enum(["development", "production"]).default("development"),
   DATABASE_URL: z.string().url().min(1),
   PORT: z.string().min(1),
+
+  //AWS
+  AWS_ACCESS_KEY_ID: z.string().min(1),
+  AWS_SECRET_ACCESS_KEY: z.string().min(1),
+  AWS_BUCKET: z.string().min(1),
+  AWS_REGION: z.string().min(1),
 });
 
 export const env = envSchema.parse(process.env);
---
+++ apps/server/src/shared/s3/s3-url-const.ts
@@ -0,0 +1,1 @@
+export const S3_URL_CONST = "https://lapes-food.s3.us-east-1.amazonaws.com/";
